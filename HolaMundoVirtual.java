99a42ef1dee0525cbf6c1de82913e5854c7ca8db
rigobertolopez <rpablolc.190482@gmail.com>
2016-03-15 19:58:28 -0600

    Added files via upload

---
+++ HolaMundoVirtual.java
@@ -0,0 +1,19 @@
+
+package hola.mundo.virtual;
+
+/**
+ *
+ * @author LABING14
+ */
+public class HolaMundoVirtual {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       System.out.println ("hola mundo virtual este es  mi primer proyecto en java ");
+        
+    }
+    
+}
